5f3459d1f7cccaa014478b602c52deb4c39f900c
Florent Ogoutchoro <ogoutchorof@gmail.com>
2026-04-18 11:29:00 +0200

    feat: refactor tsup configuration to separate entry points for better clarity

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig({
-  entry: ['src/index.ts', 'src/exports.ts'],
-  format: ['esm', 'cjs'],
-  dts: true,
-  splitting: false,
-  clean: true,
-  sourcemap: true,
-})
+export default defineConfig([
+  {
+    entry: ['src/exports.ts'],
+    format: ['esm', 'cjs'],
+    dts: true,
+    splitting: false,
+    clean: true,
+    sourcemap: true,
+  },
+  {
+    entry: ['src/index.ts'],
+    format: ['esm'],
+    dts: true,
+    splitting: false,
+    sourcemap: true,
+  },
+])
